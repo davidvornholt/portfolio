1aecaabf0fa985ab304dae12e115391185188940
David <david@vornholt.online>
2025-12-23 13:48:23 +0100

    feat(homepage): set up basic homepage structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
-import { ComponentExample } from "@/components/component-example";
+'use client';
+
+import { Button } from '@/shared/ui/presentation/components/button';
 
 export default function Page() {
-return <ComponentExample />;
+  return (
+    <section className="h-screen p-24">
+      <h2 className="text-7xl">Test 123</h2>
+      <Button onClick={() => {}}>Test 123</Button>
+    </section>
+  );
 }
